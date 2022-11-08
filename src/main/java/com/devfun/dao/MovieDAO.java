3e068883e1e078574436549d3a925d78a0b8eb1a
suk13574 <suk13574@naver.com>
2022-11-08 19:07:54 +0900

    [추가-mariaDB 테스트] MVC 모델로 DB데이터 조회 후  웹으로 출력
    Repository - MariaDB에 있는 Movie 테이블 가져옴
    new file:   src/main/java/com/devfun/dao/MovieDAO.java
    new file:   src/main/java/com/devfun/dao/MovieDAOlmpl.java
    
    Service - MovieDAO 통해 Movie 데이터 조회
    new file:   src/main/java/com/devfun/service/MovieService.java
    new file:   src/main/java/com/devfun/service/MovieServiceImpl.java
    
    Controller - 기존 home 변
    modified:   src/main/java/com/devfun/settingweb/HomeController.java
    
    select로 받아온 데이터 객체로 저장
    new file:   src/main/java/com/devfun/vo/MovieVO.java

---
+++ src/main/java/com/devfun/dao/MovieDAO.java
@@ -0,0 +1,9 @@
+package com.devfun.dao;
+
+import java.util.List;
+
+import com.devfun.vo.MovieVO;
+
+public interface MovieDAO {
+	public List<MovieVO> selectMove() throws Exception;
+}
---
+++ src/main/java/com/devfun/vo/MovieVO.java
@@ -0,0 +1,38 @@
+package com.devfun.vo;
+
+
+public class MovieVO {
+//	private int movie_id;
+	private String movie_name;
+	private String director;
+	private String types;
+	
+	
+//	public int getMovie_id() {
+//		return movie_id;
+//	}
+//	public void setMovie_id(int movie_id) {
+//		this.movie_id = movie_id;
+//	}
+	
+	public String getMovie_name() {
+		return this.movie_name;
+	}
+	public void setMovie_name(String movie_name) {
+		this.movie_name = movie_name;
+	}
+	
+	public String getDirector() {
+		return this.director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	
+	public String getTypes() {
+		return this.types;
+	}
+	public void setTypes(String types) {
+		this.types = types;
+	}
+}
